b05e7fa3a9da224340a47bcacbc8ba3308d5045e
Max Wilson <maxjw98@gmail.com>
2024-01-06 01:35:48 +0000

    Added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,5 +17,25 @@
 	});
 </script>
 
-<slot />
 <SvelteToast />
+
+<slot />
+
+<div class="footer footer-center p-10">
+	<aside class="items-center grid-flow-col">
+		<a class="link" href="https://maxwilson.co.uk" target="_blank"
+			><img src="icons/logo.svg" class="fill-current" width="36" height="36" /></a
+		>
+		<p>
+			Developed for fun by <a class="link" href="https://maxwilson.co.uk" target="_blank"
+				>Max Wilson</a
+			>
+		</p>
+	</aside>
+</div>
+
+<style>
+	.footer {
+		background-color: #0d1220;
+	}
+</style>
